715132eb86f312395cbfb4b030a5531e3d52744c
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-05-18 11:46:39 +0100

    js/getUrlParams: log helpful error for local testing

--- helpers/getUrlParams.js
+++ helpers/getUrlParams.js
@@ -3,6 +3,12 @@ export const getUrlParams = () => {
   const contractAddress = url.searchParams.get("address")
   const editionNumber = url.searchParams.get("id")
   const seed = url.searchParams.get("seed")
+
+  // log an error if no url params are found
+  if(!contractAddress || !editionNumber){
+    console.error("Url params not found :( if you are testing locally try visiting:", "http://localhost:3000/?id=1&seed=5&address=0x8f66a247c29a2e4b32da14d94ee96fcae4964370")
+  }
+
   return {
     contractAddress,
     editionNumber,
--- main.js
+++ main.js
@@ -11,11 +11,6 @@ import {
   // get url params
   const {contractAddress, editionNumber, seed} = getUrlParams()
 
-  // logs an error if no url params are found
-  if(!contractAddress || !editionNumber){
-    console.error("Url params not found :( if you are testing locally try visiting:", "http://localhost:3000/?id=1&seed=5&address=0x8f66a247c29a2e4b32da14d94ee96fcae4964370")
-  }
-
   // fallback to edition number if seed param not present
   const theSeed = seed != null ? seed : editionNumber
 
