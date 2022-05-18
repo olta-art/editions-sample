5c4ce9c0b66e895d0f2e9287e1403a460cb0eaf3
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-05-18 10:32:08 +0100

    js/getUrlParams: add getter for seed param

--- helpers/getUrlParams.js
+++ helpers/getUrlParams.js
@@ -2,8 +2,10 @@ export const getUrlParams = () => {
   const url = new URL(window.location.href)
   const contractAddress = url.searchParams.get("address")
   const editionNumber = url.searchParams.get("id")
+  const seed = url.searchParams.get("seed")
   return {
     contractAddress,
-    editionNumber
+    editionNumber,
+    seed
   }
 }
