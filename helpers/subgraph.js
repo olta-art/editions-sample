ba97415ca1e2b1cb34d18d2475a1d188e60958e2
george-e-d-g-e <george@e-d-g-e.co.uk>
2022-05-21 00:55:23 +0100

    js/subgraph: add example querys

--- helpers/subgraph.js
+++ helpers/subgraph.js
@@ -29,3 +29,165 @@ export const fetchTokens = async (contractAddress) => {
     return []
   }
 }
+
+
+/* ----------------------------------------------------------------------- */
+
+// EXAMPLES
+
+// fetch a specific token
+export const fetchToken = async (contractAddress, id) => {
+  try{
+    const result = await client.query(`
+      query{
+        token(id: "${contractAddress}-${id}") {
+          tokenURI
+          seed
+          id
+          editionNumber
+          createdAtTransactionHash
+          createdAtTimestamp
+          createdAtBlockNumber
+          burnedAtTimeStamp
+          burnedAtBlockNumber
+          owner {
+            id
+          }
+          prevOwner {
+            id
+          }
+          transfers {
+            from {
+              id
+            }
+            to {
+              id
+            }
+          }
+        }
+      }
+    `
+    ).toPromise()
+    return result.data.token
+  } catch {
+    console.error("Error")
+    return []
+  }
+}
+
+// fetch a specific token from seed
+export const fetchTokenFromSeed = async (contractAddress, seed) => {
+  try{
+    const result = await client.query(`
+      query{
+        tokens(where: {tokenContract: "${contractAddress}" seed: "${seed}"}) {
+          tokenURI
+          seed
+          id
+          editionNumber
+          createdAtTransactionHash
+          createdAtTimestamp
+          createdAtBlockNumber
+          burnedAtTimeStamp
+          burnedAtBlockNumber
+          owner {
+            id
+          }
+          prevOwner {
+            id
+          }
+          transfers {
+            from {
+              id
+            }
+            to {
+              id
+            }
+          }
+        }
+      }
+    `
+    ).toPromise()
+    return result.data.tokens[0]
+  } catch {
+    console.error("Error")
+    return []
+  }
+}
+
+// fetch token contract
+export const fetchTokenContract = async (contractAddress) => {
+  try{
+    const result = await client.query(`
+      query{
+        tokenContract(id: "${contractAddress}") {
+        createdAtBlockNumber
+        createdAtTimestamp
+        creator {
+          id
+        }
+        creatorRoyaltyBPS
+        description
+        editionSize
+        id
+        implementation
+        lastAddedVersion {
+          createdAtBlockNumber
+          createdAtTimestamp
+          id
+          label
+          animation {
+            url
+            hash
+          }
+          image {
+            url
+            hash
+          }
+        }
+        name
+        tokenContractId
+        totalBurned
+        totalMinted
+        totalSupply
+        tokens {
+          id
+        }
+      }
+    `
+    ).toPromise()
+    return result.data.tokenContract
+  } catch {
+    console.error("Error")
+    return []
+  }
+}
+
+// fetch user
+// collection = tokens[]
+// creations = tokenContracts[]
+export const fetchUser = async (userId) => {
+  try{
+    const result = await client.query(`
+      query{
+        user(id:"${userId}") {
+        collection{
+          id
+          seed
+          editionNumber
+        }
+        creations {
+          id
+          editionSize
+          name
+          description
+        }
+      }
+    `
+    ).toPromise()
+    return result.data.user
+  } catch {
+    console.error("Error")
+    return []
+  }
+}
